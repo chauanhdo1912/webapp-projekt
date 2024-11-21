e5360402a116d1c549fc523d1b897437847e06ff
Yasin Cherif <>
2024-11-21 19:20:01 +0100

    Erste Seite

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('Hauptseite.html')
 
+@app.route('/Testseite')
+def Test_Seite():
+    return render_template('Testseite.html')
+
+@app.route('/Infoseite')
+def Info_Seite():
+    return render_template('Infoseite.html')
+
+@app.route('/Login')
+def Login():
+    return render_template('Login.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
